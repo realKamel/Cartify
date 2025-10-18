63515f60e4fd5be0fee500a908915e7b7275647e
Abdelrahman <abdo.aly.k.m@gmail.com>
2025-10-19 00:31:47 +0300

    Serilog Configuration

--- Cartify.Web/Program.cs
+++ Cartify.Web/Program.cs
@@ -1,4 +1,5 @@
 using Scalar.AspNetCore;
+using Serilog;
 
 namespace Cartify.Web;
 
@@ -6,34 +7,62 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
-        
-        builder.Configuration
-            .AddJsonFile("appsettings.json", optional: false)
-            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
-            .AddEnvironmentVariables(); // Important!
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+        //initial logging for application startup
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateBootstrapLogger();
+        try
         {
-            app.MapOpenApi();
-            app.MapScalarApiReference();
+            Log.Information("Starting web host.");
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Configuration
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            //full Serilog setup after startup
+            // no use for builder.Logging.ClearProviders(); and builder.Logging.AddSerilog(logger);
+            // here as UseSerilog handles that.
+            builder.Host.UseSerilog((context, services, configuration) =>
+                configuration
+                    .ReadFrom.Configuration(context.Configuration)
+                    .ReadFrom.Services(services)
+            );
+            
+            // Add services to the container.
+            builder.Services.AddControllers();
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+            Log.Information("Web application built successfully.");
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                app.MapScalarApiReference();
+            }
+
+            app.UseSerilogRequestLogging();
+
+            app.UseHttpsRedirection();
+
+            app.UseHsts();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
         }
-        app.UseHttpsRedirection();
-        app.UseHsts();
-        app.UseAuthorization();
-
-
-        app.MapControllers();
-
-        app.Run();
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Host terminated unexpectedly!");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
     }
 }

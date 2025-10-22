fff81c978349c5061aa65850359e15159571c870
Abdelrahman <abdo.aly.k.m@gmail.com>
2025-10-23 02:08:12 +0300

    Core Entites Configuration and Migration

---
+++ Cartify.Persistence/AppData/Configurations/BrandCategoryConfiguration.cs
@@ -0,0 +1,21 @@
+using Cartify.Domain.Entities.JoinEntities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Cartify.Persistence.AppData.Configurations;
+
+public class BrandCategoryConfiguration : IEntityTypeConfiguration<BrandCategory>
+{
+    public void Configure(EntityTypeBuilder<BrandCategory> builder)
+    {
+        builder.HasKey(e => new { e.BrandId, e.CategoryId });
+
+        builder.HasOne(x => x.Brand)
+            .WithMany(x => x.BrandCategories)
+            .HasForeignKey(x => x.BrandId);
+        
+        builder.HasOne(x => x.Category)
+            .WithMany(x => x.BrandCategories)
+            .HasForeignKey(x => x.CategoryId);
+    }
+}
---
+++ Cartify.Persistence/AppData/Configurations/ProductConfiguarion.cs
@@ -0,0 +1,24 @@
+using Cartify.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Cartify.Persistence.AppData.Configurations;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Price).HasColumnType("decimal(10,2)");
+
+        builder
+            .HasOne(x => x.Brand)
+            .WithMany(x => x.Products)
+            .HasForeignKey(x => x.BrandId);
+
+        builder
+            .HasOne(x => x.Category)
+            .WithMany(x => x.Products)
+            .HasForeignKey(x => x.CategoryId);
+    }
+}

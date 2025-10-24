6b5104947b0624d5bc58d3e2e03ad5515ea006a0
Abdelrahman <abdo.aly.k.m@gmail.com>
2025-10-25 01:02:25 +0300

    Implementing Specification

---
+++ Cartify.Domain/Interfaces/ISpecification.cs
@@ -0,0 +1,18 @@
+using System.Linq.Expressions;
+using System.Numerics;
+using Cartify.Domain.Entities;
+
+namespace Cartify.Domain.Interfaces;
+
+public interface ISpecification<TEntity, TKey> where TEntity : BaseEntity<TKey> where TKey : INumber<TKey>
+{
+    Expression<Func<TEntity, bool>>? Criteria { get; }
+
+    ICollection<Expression<Func<TEntity, object>>>? RelatedDataIncludes { get; }
+    Expression<Func<TEntity, object>>? OrderByExpression { get; }
+    Expression<Func<TEntity, object>>? OrderByDescExpression { get; }
+
+    public void AddRelatedDataInclude(Expression<Func<TEntity, object>> expression);
+    public void AddOrderBy(Expression<Func<TEntity, object>> expression);
+    public void AddOrderDescBy(Expression<Func<TEntity, object>> expression);
+}
---
+++ Cartify.Persistence/QueryBuilder.cs
@@ -0,0 +1,39 @@
+using System.Numerics;
+using Cartify.Domain.Entities;
+using Cartify.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cartify.Persistence;
+
+public static class QueryBuilder
+{
+    public static IQueryable<TEntity> CreateSpecificationQuery<TEntity, TKey>(IQueryable<TEntity> initQuery,
+        ISpecification<TEntity, TKey> specification) where TEntity : BaseEntity<TKey> where TKey : INumber<TKey>
+    {
+        var query = initQuery;
+
+        if (specification.Criteria is not null)
+        {
+            query = query.Where(specification.Criteria);
+        }
+
+        if (specification.RelatedDataIncludes is not null)
+        {
+            query = specification.RelatedDataIncludes
+                .Aggregate(query, (currentQuery, item) =>
+                    currentQuery.Include(item));
+        }
+
+        if (specification.OrderByExpression is not null)
+        {
+            query = query.OrderBy(specification.OrderByExpression);
+        }
+
+        if (specification.OrderByDescExpression is not null)
+        {
+            query = query.OrderByDescending(specification.OrderByDescExpression);
+        }
+
+        return query;
+    }
+}

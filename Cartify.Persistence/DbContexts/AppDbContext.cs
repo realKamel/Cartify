ef414650eb9a410294d69aa395d78e98fca646c6
Abdelrahman <abdo.aly.k.m@gmail.com>
2025-11-03 15:58:57 +0200

    Fix to C# Reflection implementation

--- Cartify.Persistence/DbContexts/AppDbContext.cs
+++ Cartify.Persistence/DbContexts/AppDbContext.cs
@@ -1,3 +1,4 @@
+using Cartify.Persistence.AppData.Configurations;
 using Microsoft.EntityFrameworkCore;
 
 namespace Cartify.Persistence.DbContexts;
@@ -7,6 +8,10 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        //modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        modelBuilder.ApplyConfiguration(new ProductConfiguration());
+        modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+        modelBuilder.ApplyConfiguration(new BrandConfiguration());
+        modelBuilder.ApplyConfiguration(new BrandCategoryConfiguration());
     }
 }

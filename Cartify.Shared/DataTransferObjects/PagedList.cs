42a8873cc426cc4df005d0eb1dae1286738ff452
Abdelrahman <abdo.aly.k.m@gmail.com>
2025-11-03 20:33:21 +0200

    Enforce setting properties

--- Cartify.Shared/DataTransferObjects/PagedList.cs
+++ Cartify.Shared/DataTransferObjects/PagedList.cs
@@ -3,9 +3,9 @@ namespace Cartify.Shared.DataTransferObjects;
 public class PagedList<T>
 {
     public required IList<T> Data { get; init; }
-    public int Page { get; init; }
-    public int Limit { get; init; }
-    public int Total { get; init; }
+    public required int Page { get; init; }
+    public required int Limit { get; init; }
+    public required int Total { get; init; }
 
     public int TotalPages => (int)Math.Ceiling((double)Total / Limit);
     public bool HasNextPage => Page * Limit < Total;
